54ba3d7d209970f2e682ea17fcb5914604eece4d
韓文皓 <hanwenhao@hanwenhaodeMacBook-Air.local>
2024-03-23 17:20:56 +0900

    Add response model to list_products endpoint

--- api/routers/products.py
+++ api/routers/products.py
@@ -1,11 +1,13 @@
-from fastapi import APIRouter
+from typing import List
 
+from fastapi import APIRouter
+import api.schemas.products as products_schema 
 router = APIRouter()
 
 
-@router.get("/products")
+@router.get("/products", response_model=List[products_schema.Product])
 async def list_products():
-    pass
+    return [products_schema.Product(id=1)]
 
 
 @router.post("/products")
