b0ed0aab0a7085f980dba30927c20938f88124d3
韓文皓 <hanwenhao@hanwenhaodeMacBook-Air.local>
2024-03-23 16:38:43 +0900

    Add product schema models for iPhone, iPad, Mac

---
+++ api/schemas/products.py
@@ -0,0 +1,32 @@
+from pydantic import BaseModel
+from typing import List
+
+# iPhoneモデル
+class IPhone(BaseModel):
+    model: str
+    color: str
+    storage_capacity_gb: int
+    price: float
+
+# iPadモデル
+class IPad(BaseModel):
+    model: str
+    generation: int
+    color: str
+    storage_capacity_gb: int
+    price: float
+
+# Macモデル
+class Mac(BaseModel):
+    model: str
+    year: int
+    color: str
+    storage_capacity_gb: int
+    price: float
+
+# Productモデル
+class Product(BaseModel):
+    # サブモデルのリスト
+    iphones: List[IPhone] = []
+    ipads: List[IPad] = []
+    macs: List[Mac] = []
