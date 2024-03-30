90aec4e5b876512b280a785995978643e3a57b5f
韓文皓 <hanwenhao@hanwenhaodeMacBook-Air.local>
2024-03-30 21:52:09 +0900

    Add task CRUD operations and schemas for tasks

--- api/routers/task.py
+++ api/routers/task.py
@@ -2,26 +2,26 @@ from typing import List
 
 from fastapi import APIRouter
 
-import api.schemas.products as products_schema
+import api.schemas.task as task_schema
 
 router = APIRouter()
 
 
-@router.get("/products", response_model=List[products_schema.Product])
-async def list_products():
-    pass
+@router.get("/tasks", response_model=List[task_schema.Task])
+async def list_tasks():
+    return [task_schema.Task(id=1, title="1つ目のTODOタスク")]
 
 
-@router.post("/products")
-async def create_product():
-    pass
+@router.post("/tasks", response_model=task_schema.TaskCreateResponse)
+async def create_task(task_body: task_schema.TaskCreate):
+    return task_schema.TaskCreateResponse(id=1, **task_body.dict())
 
 
-@router.put("/products/{product_id}")
-async def update_product():
-    pass
+@router.put("/tasks/{task_id}", response_model=task_schema.TaskCreateResponse)
+async def update_task(task_id: int, task_body: task_schema.TaskCreate):
+    return task_schema.TaskCreateResponse(id=task_id, **task_body.dict())
 
 
-@router.delete("/products/{product_id}")
-async def delete_product():
-    pass
+@router.delete("/tasks/{task_id}", response_model=None)
+async def delete_task(task_id: int):
+    return
