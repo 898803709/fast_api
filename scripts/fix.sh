2cf30d16b397b58753e669fc8013336572afc066
韓文皓 <hanwenhao@hanwenhaodeMacBook-Air.local>
2024-03-31 20:43:46 +0900

    Refactor async functions for readability and consistency

--- scripts/fix.sh
+++ scripts/fix.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-black .
-pflake8 .
-isort .
-mypy --ignore-missing-imports .
-ruff check .   # Lint all files in the current directory.
-ruff format .  # Format all files in the current directory.
+poetry run black .
+poetry run pflake8 .
+poetry run isort .
+poetry run mypy --ignore-missing-imports .
+poetry run ruff check .   # Lint all files in the current directory.
+poetry run ruff format .  # Format all files in the current directory.
