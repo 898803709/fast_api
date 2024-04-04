75d278791327a86dac2c24632ec4c453b0195f1a
韓文皓 <hanwenhao@hanwenhaodeMacBook-Air.local>
2024-04-04 22:51:16 +0900

    Add support for mypy static type checker

--- scripts/check.sh
+++ scripts/check.sh
@@ -9,18 +9,29 @@ fi
 
 case $CHECK in
   "black")
+    echo "Pythonコードスタイルのチェック中(black)..."
     poetry run black --diff .
     ;;
   "flake8")
+    echo "Pythonコードスタイルのチェック中(flake8)..."
     poetry run pflake8 .
     ;;
   "isort")
+    echo "Pythonコードスタイルのチェック中(isort)..."
     poetry run isort --check-only .
     ;;
+  "mypy")
+    echo "Pythonの型チェック中(mypy)..."
+    poetry run mypy --ignore-missing-imports .
+    ;;
   *)
+    echo "Pythonコードスタイルのチェック中(black)..."
     poetry run black --diff .
+    echo "Pythonコードスタイルのチェック中(flake8)..."
     poetry run pflake8 .
+    echo "Pythonコードスタイルのチェック中(isort)..."
     poetry run isort --check-only .
+    echo "Pythonの型チェック中(mypy)..."
     poetry run mypy --ignore-missing-imports .
     ;;
 esac
